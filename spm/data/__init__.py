27d3acf960f35ee7dcc52724742554029f2daaf0
Liang Shuailong <liangshuailong@gmail.com>
2019-04-12 21:22:22 +0800

    add bert model

---
+++ spm/data/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author: Shuailong
+# @Email: liangshuailong@gmail.com
+# @Date:   2019-04-02 18:03:50
+# @Last Modified by:  Shuailong
+# @Last Modified time: 2019-04-02 18:04:12
+
---
+++ spm/data/dataset_readers/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author: Shuailong
+# @Email: liangshuailong@gmail.com
+# @Date:   2019-04-06 17:08:14
+# @Last Modified by:  Shuailong
+# @Last Modified time: 2019-04-06 23:38:52
+
+from spm.data.dataset_readers.snli import SnliReader
